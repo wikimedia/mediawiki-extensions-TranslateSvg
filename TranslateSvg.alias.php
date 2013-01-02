bc679f2333ecc81bb44258f2b805e8445277505c
raymond <raimond.spekking@gmail.com>
2013-01-02 09:36:54 +0100

    Add special page alias file per I90849cd3
    PS2: Fix c&p error
    
    Change-Id: I7c5edf4e14510ccad31432c4868c4b7b2184a939

---
+++ TranslateSvg.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Allows the special page title to be translated to another language.
+ * The page title can be customized into another language
+*/
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'TranslateNewSVG' => array( 'TranslateNewSVG' ),
+);
