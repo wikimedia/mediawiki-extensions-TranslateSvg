56dfbff7c40e73584237d588f51fdc028bff78b7
Umherirrender <umherirrender_de.wp@web.de>
2021-12-06 17:12:51 +0100

    Emit deprecation warning from php entry point
    Change-Id: I54266c05b1b76a705bd2dcd79db628ad99aadda2

--- TranslateSvg.php
+++ TranslateSvg.php
@@ -21,12 +21,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'TranslateSvg' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['TranslateSvg'] = __DIR__ . '/i18n';
-	/* wfWarn(
-		'Deprecated PHP entry point used for TranslateSvg extension. '.
+	wfWarn(
+		'Deprecated PHP entry point used for TranslateSvg extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the TranslateSvg extension requires MediaWiki 1.29+' );
+	die( 'This version of the TranslateSvg extension requires MediaWiki 1.35+' );
 }
